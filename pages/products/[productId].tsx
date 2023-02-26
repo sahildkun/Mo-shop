926613bb38d3c32a85896338e2590dabe25d8614
sahildkun <sahilsahoo23@gmail.com>
2023-02-26 12:05:40 +0530

    useSwr + loading 🧑‍💻

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -1,29 +1,43 @@
 import React from 'react'
-import axios from 'axios';
+import Head from 'next/head';
 import { Layout } from '@/components/layout';
 import { useRouter } from 'next/router';
 import { getAllProducts } from '@/helpers/api-utils';
 import Image from 'next/image';
+import useSWR from 'swr'
+
 
 const ProductDetail = (props) => {
 
   //  const products = useContext(CartContext);
   //  const productArray = Object.keys(products);
-  const {products} = props;
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+  const { data, error, isLoading } = useSWR('https://api.pujakaitem.com/api/products',fetcher);
+   console.log(data)
+  // const {products} = props;
   const router = useRouter();
-  console.log(router.query.productId);
+
   const productId = router.query.productId;
 
-  if(!products){
+  if(isLoading || !data){
 
-    return <h1>Loading...</h1>
+    return(
+    <>
+    <Layout>
+    <h1>Loading...</h1>
+    </Layout>
+    </>)
   }
-  const product = products.find((product) => product.id === productId);
+
+  const product = data.find((product) => product.id === productId);
 
 
 
   return (
     <>
+    <Head>
+    <title>{product.name}</title>
+    </Head>
    <Layout>
      <div className='grid grid-cols-2 m-10'>
        <div>
@@ -37,17 +51,18 @@ const ProductDetail = (props) => {
 
     </>
   )
+
 }
 
 
 
-export async function getServerSideProps(context) {
-  const products = await getAllProducts();
+// export async function getServerSideProps(context) {
+//   const products = await getAllProducts();
 
-  return {
-      props: {
-      products: products,
-    }
-  }
-}
+//   return {
+//       props: {
+//       products: products,
+//     }
+//   }
+// }
 export default ProductDetail;
